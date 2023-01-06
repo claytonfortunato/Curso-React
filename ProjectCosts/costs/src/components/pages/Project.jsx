060836e2cee4a543b2b73ea3598744cab3d1f473
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2023-01-06 00:26:22 -0300

    Costs project

--- ProjectCosts/costs/src/components/pages/Project.jsx
+++ ProjectCosts/costs/src/components/pages/Project.jsx
@@ -98,14 +98,37 @@ const Project = () => {
       body: JSON.stringify(project),
     })
       .then((resp) => resp.json())
-      .then((data) => {
-        // exibir os serviços
-        console.log(data);
+      .then(() => {
+        setShowServiceForm(false);
       })
       .catch((err) => console.log(err));
   };
 
-  const removeService = () => {};
+  const removeService = (id, cost) => {
+    const servicesUpdate = project.services.filter(
+      (service) => service.id !== id
+    );
+
+    const projectUpdated = project;
+
+    projectUpdated.services = servicesUpdate;
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectUpdated),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(projectUpdated);
+        setServices(servicesUpdate);
+        setMessage("Serviço removido com sucesso");
+      })
+      .catch((err) => console.log(err));
+  };
 
   const toggleProjectForm = () => {
     setShowProjectForm(!showProjectForm);
