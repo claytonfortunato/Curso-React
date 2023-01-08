69bfe11ec624480c634eeae7c54724d340deed63
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2023-01-08 13:54:07 -0300

    carousel

--- Carousel/carrosel/src/App.jsx
+++ Carousel/carrosel/src/App.jsx
@@ -1,35 +1,66 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
   useEffect(() => {
-    fetch("http://localhost:3000/static/shoes.json")
-      .then((response) => response.json())
-      .then(console.log);
+    fetch("http://localhost:5173/src/static/shoes.json")
+      .then((resp) => resp.json())
+      .then(setData);
   }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null;
 
   return (
     <div className="container">
       <div className="logo">
         <img src="/src/static/images/super-shoes.png" alt="Super Shoes Logo" />
       </div>
-      <div className="carousel">
-        <div className="item">
-          <div className="image">
-            <img
-              src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg"
-              alt="Shoe"
-            />
-          </div>
-          <div className="info">
-            <span className="name">Super Shoe 1</span>
-            <span className="oldPrice">U$ 299.00</span>
-            <span className="price">U$ 199.00</span>
-          </div>
-        </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">{oldPrice}</span>
+                <span className="price">{price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img
+            src="/src/static/images/216151_right_chevron_icon.png"
+            alt="Scroll Left"
+          />
+        </button>
+        <button onClick={handleRightClick}>
+          <img
+            src="/src/static/images/216151_right_chevron_icon.png"
+            alt="Scroll Right"
+          />
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
