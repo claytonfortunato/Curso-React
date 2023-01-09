5e932219933e8c5d18a7be6d32780fca52a0cbce
Clayton Fortunato <claytonreis10@gmail.com>
2023-01-09 13:47:20 -0300

    projeto modal

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -9,7 +9,24 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setIsModalVisible(true)}>Open</button>
-      {isModalVisible ? <Modal /> : null}
+      {isModalVisible ? (
+        <Modal onClose={() => setIsModalVisible(false)}>
+          <h2>Modal App</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            magnam ipsum, nesciunt voluptatem pariatur odit harum labore ipsam
+            laborum accusamus animi eligendi in asperiores necessitatibus
+            dolores dignissimos saepe dolore adipisci. Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Quos tempora nulla, maiores ex,
+            perferendis quam placeat laboriosam quaerat rerum, architecto magni
+            quod unde quae recusandae voluptatibus ea molestias facilis
+            obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Minima odio nostrum maxime voluptatum praesentium eligendi hic
+            reiciendis eum officia corporis? Esse accusamus enim quo corporis
+            porro blanditiis, sit aperiam eum?
+          </p>
+        </Modal>
+      ) : null}
     </div>
   );
 }
--- modal/src/components/Modal.jsx
+++ modal/src/components/Modal.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Modal = (props) => {
-  const { children } = props;
+const ESCAPE_KEYCODE = 27;
+const Modal = ({ id = "modal", onClose = () => {}, children }) => {
+  useEffect(() => {
+    if (!window) return;
+
+    const keyUpListener = (e) => {
+      if (e.keyCode === ESCAPE_KEYCODE) onClose();
+    };
+
+    window.addEventListener("keyup", keyUpListener);
+
+    return () => {
+      window.removeEventListener("keyup", keyUpListener);
+    };
+  }, []);
+
+  //Click fora do container pra fechar.
+  const handleOutsideClick = (e) => {
+    if (e.target.id === id) onClose();
+  };
 
   return (
-    <div className="modal">
-        <div className="container">
-            <button className="close"></button>
-            <div className="content">{children}
-            </div>
-        </div>
+    <div id={id} className="modal" onClick={handleOutsideClick}>
+      <div className="container">
+        <button className="close" onClick={onClose}></button>
+        <div className="content">{children}</div>
+      </div>
     </div>
-  )
-
+  );
+};
 export default Modal;
