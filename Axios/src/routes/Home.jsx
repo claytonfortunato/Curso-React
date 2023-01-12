ce05ed923ed68f66d154b159e25eadcf5f1e562d
claytonfortunato <claytonreis10@gmail.com>
2023-01-11 23:47:29 -0300

    Usando Axios para os posts

--- Axios/src/routes/Home.jsx
+++ Axios/src/routes/Home.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+import blogFetch from "../axios/config";
+
+import "./Home.css";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
+
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   );
 };
