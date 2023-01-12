8fd682db877f88aff6f66ed8e47c00f9be50d1f8
claytonfortunato <claytonreis10@gmail.com>
2023-01-11 22:09:04 -0300

    consumindo api com axios

---
+++ Axios/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
