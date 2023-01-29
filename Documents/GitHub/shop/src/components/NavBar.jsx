b3cb90bae9119a7a9a7311e8a5f162e7cd5448bc
claytonfortunato <claytonreis10@gmail.com>
2023-01-29 10:43:41 -0300

    feat:bugs fix

---
+++ Documents/GitHub/shop/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <nav className="nav-item">
+        <ul className="nav-itens">
+          <li>Sneakers Shopping</li>
+          <li>Carrinho</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
